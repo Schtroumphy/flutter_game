4a1f8c530350962365a38ec0e53df2cabea6f2b4
thessalenejean-louis <tjeanlouis@ippon.fr>
2023-10-02 09:58:57 +0200

    chore : Add stacktrace on flutter errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_game/core/constants.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 import 'core/router.dart';
 
@@ -10,6 +11,11 @@ void main() {
       child: MyApp(),
     ),
   );
+  FlutterError.demangleStackTrace = (StackTrace stack) {
+    if (stack is stack_trace.Trace) return stack.vmTrace;
+    if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
+    return stack;
+  };
 }
 
 class MyApp extends ConsumerWidget {
