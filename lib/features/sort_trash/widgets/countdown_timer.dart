eca7de6f6c06418833323b2c7e7191cd2d85cdc9
thessalenejean-louis <tjeanlouis@ippon.fr>
2023-10-05 11:36:55 +0200

    chore : Minor cleaning

--- lib/features/sort_trash/widgets/countdown_timer.dart
+++ lib/features/sort_trash/widgets/countdown_timer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_game/features/sort_trash/widgets/timer_provider.dart';
+import 'package:flutter_game/core/styles.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:timer_count_down/timer_controller.dart';
 import 'package:timer_count_down/timer_count_down.dart';
 
 class CountdownTimer extends ConsumerStatefulWidget {
@@ -15,8 +14,6 @@ class CountdownTimer extends ConsumerStatefulWidget {
 }
 
 class _CountdownTimerState extends ConsumerState<CountdownTimer> {
-  final CountdownController _controller = CountdownController(autoStart: false);
-
   final bool startTimer = false;
   double _currentWidth = 250;
 
@@ -27,63 +24,50 @@ class _CountdownTimerState extends ConsumerState<CountdownTimer> {
   }
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final startTimer = ref.watch(isTimerStartedProvider.notifier).state;
-    if(startTimer) {
-      _controller.start();
-      print("controller started");
-    }
-
-
-
     return Container(
-      margin: const EdgeInsets.all(8),
-      height: 16,
+      margin: const EdgeInsets.all(Insets.m),
+      height: 20,
       width: 250,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 1),
+        border: Border.all(color: Colors.white, width: 1),
         borderRadius: const BorderRadius.all(Radius.circular(40)),
       ),
-      child: Stack(
-        alignment: Alignment.centerLeft,
-        children: [
-          AnimatedContainer(
-            padding: const EdgeInsets.all(8),
+      child: Stack(alignment: Alignment.centerLeft, children: [
+        AnimatedContainer(
+            padding: const EdgeInsets.all(Insets.m),
             width: _currentWidth,
             duration: const Duration(seconds: 1),
             height: 50,
             decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: const BorderRadius.all(Radius.circular(40)),
-                color: Colors.blue),
-            child: const SizedBox.shrink()
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left:  8.0),
-            child: Countdown(
-              seconds: widget.duration.inSeconds,
-              build: (BuildContext context, double time) {
-                WidgetsBinding.instance.addPostFrameCallback((_){
-                  currentWidth = time * 250 / widget.duration.inSeconds;
-                });
-                return Text(
-                  "${time.toInt()}:00",
-                  style: TextStyle(
-                    color: time > 3 ? Colors.white : Colors.red,
-                    fontWeight: FontWeight.bold,
-                  ),
-                );
-              },
-              onFinished: () => widget.onFinished?.call(),
+              border: Border.all(color: Colors.black),
+              borderRadius: const BorderRadius.all(Radius.circular(40)),
+              color: Colors.blue,
             ),
+            child: const SizedBox.shrink()),
+        Padding(
+          padding: const EdgeInsets.only(left: Insets.m),
+          child: Countdown(
+            seconds: widget.duration.inSeconds,
+            interval: const Duration(milliseconds: 100),
+            build: (BuildContext context, double time) {
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                currentWidth = time * 250 / widget.duration.inSeconds;
+              });
+              return Text(
+                "$time",
+                style: TextStyle(
+                  color: time > 3 ? Colors.white : Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            },
+            onFinished: _onTimerFinished,
           ),
-        ]
-      ),
+        ),
+      ]),
     );
   }
+
+  _onTimerFinished() => widget.onFinished?.call();
 }
